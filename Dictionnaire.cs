b76b1b28e17f4fe915fd05679a2db3b99482fb2a
alex-crr <73464112+alex-crr@users.noreply.github.com>
2023-12-02 10:49:38 +0100

    Create Dictionnaire.cs
    almost lost

---
+++ Dictionnaire.cs
@@ -0,0 +1,57 @@
+﻿ using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MotsGlissés
+{
+    class Dictionnaire
+    {
+        string _chemin;
+        string[][] _lettres;
+
+        public Dictionnaire(string filePath)
+        {
+            _chemin = filePath;
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("ReadFile: file not found");
+            }
+
+            else
+            {
+                string[] readText = File.ReadAllLines(filePath);
+                if(readText.Length != 26) { throw new Exception("Dictionnaire: Invalid file structure"); }
+                for(int i = 0; i < readText.Length; i++)
+                {
+                    _lettres[i] = readText[i].Split(" ");
+                }
+            }
+        }
+
+        public void Sort()
+        {
+            for(int i = 0; i <  _lettres.Length; i++)
+            {
+                _lettres[i] = Extras.Fusion(_lettres[i]);
+            }
+        }
+
+        public bool Exists(string input)
+        {
+            if (input == null) return false;
+            if (input.Length == 0) return false;
+            if (!input.All(Char.IsLetter)) return false;
+            else
+            {
+                bool Cherche(string[] tab)
+                {
+                    if (tab.Length == 0) return false;
+                    if (tab.Length == 1) return (tab[0] == input);
+                    else
+                    {
+
+                    }
+                }
+            }
+    }
+}
