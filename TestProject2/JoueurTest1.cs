3b8b934bcd5d8109faf3adc783f918ec4f0848fa
alex-crr <73464112+alex-crr@users.noreply.github.com>
2023-12-03 20:09:23 +0100

    1st iteration

---
+++ TestProject2/JoueurTest1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+namespace TestProject2
+{
+    class JoueurTest1
+    {
+
+        [Test]
+        [TestCase("Marina")]
+        public void RechercheDico(string input)
+        {
+            Joueur joueur = new Joueur(input, 0, new List<string>());
+            Assert.That(joueur.Nom, Is.EqualTo(input));
+        }
+    }
+}
