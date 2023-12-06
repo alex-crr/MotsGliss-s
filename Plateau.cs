d3bea12ebe8632761e6c1a33dcf19c7fd35d1709
speedy27 <98963154+speedy27@users.noreply.github.com>
2023-12-06 15:17:06 +0100

    Update Plateau.cs

--- Plateau.cs
+++ Plateau.cs
@@ -6,6 +6,9 @@ namespace MotsGlissés
 {
     public class Plateau
     {
+        // faire une liste avec le nombre de lettre possible ,
+        // choisir un element au hasard
+        // et le supprimer de la liste
         char[,] plat;
         Random r = new Random();
 
@@ -41,12 +44,29 @@ namespace MotsGlissés
             }
             s.Close();
         }
-
+        /// <summary>
+        /// fonction lit le fichier et met chacun des éléments dans la plateau
+        /// </summary>
+        /// <param name="nomfile">nom du fichier</param>
         public void toRead(string nomfile)
         {
+            //pas de trycatch car le verification
+          
+            string[] lines = File.ReadAllLines(nomfile);
+            string[][] lines2 = new string[lines.Length][];
+            int var  = 0;
+            foreach (string line in lines) // remplie la lines2
+            {
+                lines2[var] = line.Split(';'); 
+            }
+            for (int i = 0; i < lines2.GetLength(0); i++)
+            {
+                for (int j = 0; j < lines2[i].GetLength(1); j++)
+                {
+                    plat[i, j] = lines2[i][j][0]; // met les éléments de lines2 dans plateau
+                }
+            }
 
         }
-
-
     }
 }
