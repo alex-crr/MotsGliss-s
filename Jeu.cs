03004ed357ce30e6a5bb3553d8819fc9cb152e58
speedy27 <98963154+speedy27@users.noreply.github.com>
2023-12-06 19:20:36 +0100

    Update Jeu.cs

--- Jeu.cs
+++ Jeu.cs
@@ -8,6 +8,29 @@ namespace MotsGlissés
 {
     public class Jeu
     {
+        private Dictionnaire dico1;
+        Plateau plateau1;
+        Joueur joueur1;
+        int tempsjoueur ;
+        int tempsjeu = 120;
 
+    }
+    public Jeu(Dictionnaire dico, Plateau plateau1,Joueur joueur1, int tempsjoueur)
+    {
+        this.dico = dico;
+        this.plateau1 = plateau1;
+        this.joueur1 = joueur1;
+        this.tempsjoueur = tempsjoueur
+
+    }
+
+
+
+    public void play()
+    {
+        DateTime startGame= DateTime.Now;
+        TimeSpan tempsEcoule = TimeSpan.Zero;
+
+        while(DateTime.Now -)
     }
 }
