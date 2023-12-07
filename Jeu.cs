8957e92ee79008172d247288c8c8f9313be83244
speedy27 <98963154+speedy27@users.noreply.github.com>
2023-12-07 11:04:23 +0100

    Update Jeu.cs

--- Jeu.cs
+++ Jeu.cs
@@ -7,6 +7,7 @@ using System.Xml.Linq;
 
 namespace MotsGlissés
 {
+  
     public class Jeu
     {
         Dictionnaire _dico;
@@ -26,28 +27,34 @@ namespace MotsGlissés
         }
 
 
-
+        /// <summary>
+        /// défini le temps de jeu les les resultats
+        /// </summary>
         public void play()
         {
-            DateTime startGame = DateTime.Now;
-            DateTime finjeu = startGame.Add(tempsjeu);
+            DateTime startGame = DateTime.Now; //temps maintenant
+            DateTime finjeu = startGame.Add(tempsjeu);//temps definie au préalable
             while (startGame < finjeu)
             {
-                foreach (Joueur j in joueur)
+                foreach (Joueur j in _joueurs)
                 {
                     Console.WriteLine($"{j.Nom} a toi de jouer !");
                     DateTime debuttour = DateTime.Now;
-                    TimeSpan tempsrestant = tempsjoueur;
+                    TimeSpan tempsrestant = _tempsjoueur;
                     string motcherche = "";
                     while (DateTime.Now < debuttour.Add(tempsrestant) && motcherche == "")
                     {
                         Console.WriteLine("entre un mot");
                         motcherche = Console.ReadLine();
-                        if (dico.RechDichoRecursif(motrecherche) && Joueur.Contient(motcherche) && Plateau.Recherche_Mot(motcherche))
+                        if (_dico.RechDichoRecursif(motcherche) && Plateau.Recherche_Mot(motcherche)) //vérifie que le mot existe dans le dictionniaire et sur le plateau
                         {
 
-                            _joueur.Add_Mot(motcherche);
-                            //joueur.score += ;
+                            j.Add_Mot(motcherche);
+                            for(int i = 0; i < motcherche.Length;i++)
+                            {
+                                // trouver la fonction
+                            }
+                            j.Add_Score += ;
                             _plateau.Maj_Plateau(motcherche);
                         }
                         else
