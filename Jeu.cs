274abe1c4b3c98144feae0c2f2672fb818f8e4e4
speedy27 <98963154+speedy27@users.noreply.github.com>
2023-12-07 09:21:56 +0100

    Update Jeu.cs

--- Jeu.cs
+++ Jeu.cs
@@ -11,11 +11,11 @@ namespace MotsGlissés
         Dictionnaire dico;
         Plateau plateau;
         Joueur joueur;
-        int tempsjoueur ;
-        int tempsjeu = 120;
+        TimeSpan tempsjoueur ;
+        TimeSpan tempsjeu = (120);
 
     }
-    public Jeu(Dictionnaire dico, Plateau plateau,Joueur joueur, int tempsjoueur)
+    public Jeu(Dictionnaire dico, Plateau plateau,Joueur joueur, TimeSpan tempsjoueur)
     {
         this.dico = dico;
         this.plateau1 = plateau;
@@ -29,17 +29,33 @@ namespace MotsGlissés
     public void play()
     {
         DateTime startGame= DateTime.Now;
-        TimeSpan tempsEcoule = TimeSpan.Zero;
+       DateTime temps = TimeSpan.Zero;
 
-        while(tempsEcoule < tempsjeu )
+        while(startGame < tempsjeu )
         {
             foreach(Joueur j in joueur)
             {
                 Console.WriteLine($"{j.Nom} a toi de jouer !");
                 DateTime debuttour = DateTime.Now;
                 TimeSpan tempsrestant = tempsjoueur;
+                string motcherche = "";
+                while(debuttour<tempsrestant && motcherche=="")
+                {
+                    Console.WriteLine("entre un mot");
+                    motcherche = Console.ReadLine();
+                    if( motcherche dans dictionnaire et existe dans plateau)
+                        {
+                            this.joueur.score = addition;
+                            supprimer/decaler matrice
+                        }
+                    else
+                    {
+                        Console.WriteLine("le mot n'existe pas")
+                    }
+                }
 
             }
+
         }
     }
 }
