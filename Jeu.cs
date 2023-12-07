776063a5596edf0c3195d813f8417158f8d3236e
speedy27 <98963154+speedy27@users.noreply.github.com>
2023-12-07 10:39:05 +0100

    Update Jeu.cs

--- Jeu.cs
+++ Jeu.cs
@@ -3,59 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MotsGlissés
 {
     public class Jeu
     {
-        Dictionnaire dico;
-        Plateau plateau;
-        List<Joueur> joueur;
-        TimeSpan tempsjoueur ;
-        TimeSpan tempsjeu = (120);
-
-    }
-    public Jeu(Dictionnaire dico, Plateau plateau,List<Joueur> joueur, TimeSpan tempsjoueur)
-    {
-        this.dico = dico;
-        this.plateau1 = plateau;
-        this.joueur = joueur;
-        this.tempsjoueur = tempsjoueur;
-
-    }
+        Dictionnaire _dico;
+        Plateau _plateau;
+        List<Joueur> _joueurs;
+        TimeSpan _tempsjoueur;
+        TimeSpan tempsjeu = TimeSpan.FromSeconds(120);
 
 
-
-    public void play()
-    {
-        DateTime startGame= DateTime.Now;
-        DateTime finjeu = startGame.Add(tempsjeu);
-        while(startGame < finjeu )
+        public Jeu(Dictionnaire dico, Plateau plateau, List<Joueur> joueur, TimeSpan tempsjoueur)
         {
-            foreach(Joueur j in joueur)
+            this._dico = dico;
+            this._plateau = plateau;
+            this._joueurs = joueur;
+            this._tempsjoueur = tempsjoueur;
+
+        }
+
+
+
+        public void play()
+        {
+            DateTime startGame = DateTime.Now;
+            DateTime finjeu = startGame.Add(tempsjeu);
+            while (startGame < finjeu)
             {
-                Console.WriteLine($"{j.Nom} a toi de jouer !");
-                DateTime debuttour = DateTime.Now;
-                TimeSpan tempsrestant = tempsjoueur;
-                string motcherche = "";
-                while(DateTime.Now<debuttour.Add(tempsrestant) && motcherche=="")
+                foreach (Joueur j in joueur)
                 {
-                    Console.WriteLine("entre un mot");
-                    motcherche = Console.ReadLine();
-                    if(dico.RechDichoRecursif(motrecherche) && Joueur.Contient(motcherche) && Plateau.Recherche_Mot(motcherche))
+                    Console.WriteLine($"{j.Nom} a toi de jouer !");
+                    DateTime debuttour = DateTime.Now;
+                    TimeSpan tempsrestant = tempsjoueur;
+                    string motcherche = "";
+                    while (DateTime.Now < debuttour.Add(tempsrestant) && motcherche == "")
                     {
+                        Console.WriteLine("entre un mot");
+                        motcherche = Console.ReadLine();
+                        if (dico.RechDichoRecursif(motrecherche) && Joueur.Contient(motcherche) && Plateau.Recherche_Mot(motcherche))
+                        {
 
-                        Joueur.Add_Mot(motcherche);
-                        joueur.score += ;
-                        supprimer / decaler matrice
+                            _joueur.Add_Mot(motcherche);
+                            //joueur.score += ;
+                            _plateau.Maj_Plateau(motcherche);
+                        }
+                        else
+                        {
+                            Console.WriteLine("le mot n'existe pas");
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine("le mot n'existe pas")
-                    }
+
                 }
-
             }
+            Console.WriteLine("jeu terminer voici les resultat");
+            foreach (Joueur j in _joueurs)
+            {
+                Console.WriteLine(j.toString);
+            }
+
 
         }
     }
